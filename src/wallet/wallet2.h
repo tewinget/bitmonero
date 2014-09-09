db4c3c060dadb27db6350fb3542530aea16e0cb7
Thomas Winget <tewinget@gmail.com>
2014-09-08 23:49:08 -0400

    change to allow (at least a bit) for multiple TXT records

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -206,7 +206,7 @@ namespace tools
     
     static std::vector<std::vector<cryptonote::tx_destination_entry>> split_amounts(std::vector<cryptonote::tx_destination_entry> dsts, size_t num_splits);
 
-    static std::string address_from_url(const std::string& url, bool& dnssec_valid);
+    static std::vector<std::string> addresses_from_url(const std::string& url, bool& dnssec_valid);
 
     static std::string address_from_txt_record(const std::string& s);
   private:
